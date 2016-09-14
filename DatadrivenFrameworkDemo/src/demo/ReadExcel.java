0eb1d07daa232c5ee1be058524076b050e415f11
Deepinder <DSB@DSB-PC>
2016-09-14 19:45:36 -0400

    Pushing the code in the central repo to share it with other team members.

---
+++ DatadrivenFrameworkDemo/src/demo/ReadExcel.java
@@ -0,0 +1,37 @@
+package demo;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class ReadExcel {
+	public static String[][] getData(String fileName, String sheetName) throws IOException {   //Defining the method
+		File file = new File("C:\\Selenium19\\TestData.xlsx");
+		FileInputStream ips = new FileInputStream(file); //For reading data. For writing data, we use - FileOutputStream.
+		Workbook Wb = new XSSFWorkbook(ips); //file is in .xlsx format . For .xls format we use HSSFWorkbook
+		Sheet Sh = Wb.getSheet(sheetName);
+		
+		int rowNum = Sh.getLastRowNum() + 1;
+		int colNum = Sh.getRow(0).getLastCellNum();
+		String[][] data = new String[rowNum][colNum];
+		
+		
+		for (int i=0; i< rowNum; i++){  //Used to iterate over rows
+			Row row = Sh.getRow(i);
+				for (int j=0; j<colNum; j++){    //Used to iterate over columns
+					String value = row.getCell(j).getStringCellValue();
+					data[i][j] = value;
+				}
+		}  
+		return data;
+		
+		
+	}
+	
+
+}
